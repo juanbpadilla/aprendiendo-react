38fac30f2c8eb2ff68bdd4af53e2b85848795986
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-09-28 04:12:11 -0400

    Extracción de la lógica del componente sobre los filtros en un Custom Hook

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -3,8 +3,7 @@ import { Products } from "./components/Products.jsx"
 import { useState } from "react"
 import { Header } from "./components/Header.jsx"
 
-function App() {
-  const [products] = useState(initialProducts)
+function useFilters () {
   const [filters, setFilters] = useState({
     category: 'all',
     minPrice: 0
@@ -22,6 +21,13 @@ function App() {
     })
   }
 
+  return { filterProducts, setFilters }
+}
+
+function App() {
+  const [products] = useState(initialProducts)
+  const { filterProducts, setFilters } = useFilters()
+  
   const filteredProducts = filterProducts(products)
 
   return (
