741e9d6539332aa892cd09499415fd92c7be45e5
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-09-28 05:46:59 -0400

    extraer el hook useFilters a un archivo separado

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,28 +1,11 @@
 import { products as initialProducts } from "./mocks/products.json"
 import { Products } from "./components/Products.jsx"
-import { useContext, useState } from "react"
+import { useState } from "react"
 import { Header } from "./components/Header.jsx"
 import { Footer } from "./components/Footer.jsx"
 import { IS_DEVELOPMENT } from "./config.js"
-import { FiltersContext } from "./context/filters.jsx"
+import { useFilters } from "./hooks/useFilters.js"
 
-function useFilters () {
-  const { filters, setFilters } = useContext(FiltersContext)
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          product.category === filters.category
-        )
-      )
-    })
-  }
-
-  return { filters, filterProducts, setFilters }
-}
 
 function App() {
   const [products] = useState(initialProducts)
