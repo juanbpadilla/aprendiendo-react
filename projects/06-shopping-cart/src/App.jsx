f4b6f3eadc87581206df12360267625e5de918d1
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-09-28 06:43:24 -0400

    Creación de un carrito desde cero

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header.jsx"
 import { Footer } from "./components/Footer.jsx"
 import { IS_DEVELOPMENT } from "./config.js"
 import { useFilters } from "./hooks/useFilters.js"
+import { Cart } from "./components/Cart.jsx"
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
       <Header />
+      <Cart />
       <Products products={filteredProducts} />
       {IS_DEVELOPMENT && <Footer />}
     </>
---
+++ projects/06-shopping-cart/src/components/Cart.jsx
@@ -0,0 +1,40 @@
+import { useId } from 'react'
+import { CartIcon, ClearCartIcon } from './Icons.jsx'
+
+export function Cart () {
+  const cartCheckBoxId = useId()
+
+  return (
+    <>
+      <label className='cart-button' htmlFor={cartCheckBoxId}>
+        <CartIcon />
+      </label>
+      <input id={cartCheckBoxId} type='checkbox' hidden />
+
+      <aside className='cart'>
+        <ul>
+          <li>
+            <img 
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Google_Pixel_and_Pixel_XL_smartphones_%2830155272575%29.jpg/800px-Google_Pixel_and_Pixel_XL_smartphones_%2830155272575%29.jpg" 
+              alt='Iphone' 
+            />
+            <div>
+              <strong>iPhone</strong> - $1499
+            </div>
+
+            <footer>
+              <small>
+                Qty: 1
+              </small>
+              <button>+</button>
+            </footer>
+          </li>
+        </ul>
+
+        <button>
+          <ClearCartIcon />
+        </button>
+      </aside>
+    </>
+  )
+}
