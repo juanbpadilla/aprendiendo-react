5983735b7073fec175dbcd1885b82b2d27e1226a
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-09-29 07:51:27 -0400

    Usar React Bootstrap

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,19 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import './App.css'
 import { useStore } from './hooks/useStore';
+import { AUTO_LANGUAGE } from './constants';
+import { ArrowsIcon } from './components/Icons';
 
 
 
 function App() {
-  const { fromLanguage,setFromLanguage } = useStore()
+  const { fromLanguage, toLanguage, interchangeLanguages } = useStore()
   return (
-    <div className='App'>
+    <Container fluid >
       <h1>Google Translate</h1>
-      <button onClick={() => {
-        setFromLanguage('es')
-      }}>Cambiar a Español</button>
-      { fromLanguage }
-    </div>
+
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+
+        <Col>
+          <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
+            <ArrowsIcon />
+          </Button>
+        </Col>
+
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
