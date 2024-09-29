1bb0bdf56c8ea77337801fa8cd48c0d82dcf0d9d
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-09-29 03:31:33 -0400

    empleamos un useReducer para extraer las acciones del CartProvider en un nuevo Hook

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -1,46 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import { cartReducer, cartInitialState } from "../reducers/cart.js";
 
 export const CartContext = createContext()
 
+function useCartReducer () {
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState)
+
+  const addToCart = product => dispatch({
+    type: 'ADD_TO_CART',
+    payload: product
+  })
+
+  const removeFromCart = product => dispatch({
+    type: 'REMOVE_FROM_CART',
+    payload: product
+  })
+
+  const clearCart = () => dispatch({ type: 'CLEAR_CART' })
+
+  return { state, addToCart, removeFromCart, clearCart }
+}
+
 export function CartProvider ({ children }) {
-  const [cart, setCart] = useState([])
-
-  const addToCart = product => {
-    // setCart([...cart, product]) de la manera sencilla
-
-    //  Check if the product is already in the cart
-    const productInCartIndex = cart.findIndex(item => item.id === product.id)
-
-    if (productInCartIndex >= 0) {
-      //  una forma sería usando structuredClone
-      // structuredClone hace copias profundas de los arrays y objetos
-      const newCart = structuredClone(cart)
-      newCart[productInCartIndex].quantity += 1
-
-      return setCart(newCart)
-    }
-
-    // producto no está en el carrito
-    setCart(prevState => ([
-      ...prevState,
-      {
-        ...product,
-        quantity: 1
-      }
-    ]))
-  }
-
-  const removeFromCart = product => {
-    setCart(prevState => prevState.filter(item => item.id != product.id))
-  }
-
-  const clearCart = () => {
-    setCart([])
-  }
+  const { state, addToCart, removeFromCart, clearCart } = useCartReducer()
 
   return (
     <CartContext.Provider value={{ 
-      cart,
+      cart: state,
       addToCart,
       removeFromCart,
       clearCart
