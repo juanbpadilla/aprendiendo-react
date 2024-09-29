9961f416e684d8a732de4ed76fa88d58f964e927
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-09-29 09:17:05 -0400

    TextArea con estilos | 1-2

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form, Stack } from 'react-bootstrap';
 import './App.css'
 import { useStore } from './hooks/useStore';
 import { AUTO_LANGUAGE } from './constants';
@@ -13,31 +13,44 @@ function App() {
   const { fromLanguage, toLanguage, interchangeLanguages, setFromLanguage, setToLanguage } = useStore()
   return (
     <Container fluid >
-      <h1>Google Translate</h1>
+      <h2>Google Translate</h2>
 
       <Row>
         <Col>
-          <LanguageSelector 
-            type={SectionType.From}
-            value={fromLanguage}
-            onChange={setFromLanguage} 
-          />
-          {fromLanguage}
+          <Stack gap={2}>
+            <LanguageSelector 
+              type={SectionType.From}
+              value={fromLanguage}
+              onChange={setFromLanguage} 
+            />
+            <Form.Control 
+              as='textarea'
+              placeholder='Introducir texto'
+              autoFocus
+              style={{ height: '150px' }}
+            />
+          </Stack>
         </Col>
 
-        <Col>
+        <Col xs='auto' >
           <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages}>
             <ArrowsIcon />
           </Button>
         </Col>
 
         <Col>
+        <Stack gap={2}>
           <LanguageSelector 
             type={SectionType.To}
             value={toLanguage}
             onChange={setToLanguage}
           />
-          {toLanguage}
+          <Form.Control 
+            as='textarea'
+            placeholder='Traducción'
+            style={{ height: '150px' }}
+          />
+        </Stack>
         </Col>
       </Row>
     </Container>
